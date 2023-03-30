3c4ce1da22cfe69f5678701b31a8a8898acc79ed
vzhaivoronok <vladislava99912@gmail.com>
2023-03-30 13:37:14 +0300

    homework

---
+++ untitled/src/Homework.java
@@ -0,0 +1,11 @@
+public class Homework {
+    public int a = 5;
+
+    public int getA() {
+        System.out.println(a);
+        return a;
+    }
+
+
+
+}
---
+++ untitled/src/Main.java
@@ -0,0 +1,6 @@
+public class Main {
+    public static void main(String[] args) {
+       Homework homeworkA = new Homework();
+       System.out.println(homeworkA.getA());
+    }
+}
